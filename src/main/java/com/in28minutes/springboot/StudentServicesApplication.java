18f5bf052dd417b2a5ca2dd8dcc128b1f9f0026f
oOmegaa <lordvanbrocken@gmail.com>
2020-05-20 20:44:23 -0300

    Update StudentServicesApplication.java

--- src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentServicesApplication {
 
+	// this is a comment
 	public static void main(String[] args) {
 		SpringApplication.run(StudentServicesApplication.class, args);
 	}
